9af0aa11f00d328e0375cab9366e963bcd8218dc
Aiatur <aiatur00@gmail.com>
2020-06-09 20:24:05 +0530

    All File Copied
    Signed-off-by: Aiatur <aiatur00@gmail.com>

---
+++ src/com/test/pack/aa/AA.java
@@ -0,0 +1,19 @@
+package com.test.pack.aa;
+
+public class AA {
+
+
+	public static void main(String[] args) {
+		int i=0;
+		int j=1;
+		String str ="The ans";
+		do {
+			System.out.println(str+ ++i+j);
+			break;
+		}
+
+
+	}
+
+
+}
---
+++ src/com/test/pack/bb/BB.java
@@ -0,0 +1,5 @@
+package com.test.pack.bb;
+
+public class BB {
+
+}
